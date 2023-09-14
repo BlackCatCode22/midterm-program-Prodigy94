594ccb8fb93de697145c65f22cef441b03e0b9d1
Antonio Garcia <124105810+Prodigy94@users.noreply.github.com>
2023-09-14 12:08:55 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,29 +1,14 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+
 public class Main {
     public static void main(String[] args) {
 
-        Student myStudent = new Student();
-        myStudent.firstName = "Jim";
-        myStudent.lastName = "Halpert";
-        myStudent.major = "Business";
-        myStudent.gpa = 2.3;
-        myStudent.age  = 24;
-        myStudent.onProbation = false;
 
-        Student myStudent2 = new Student();
-        myStudent2.firstName = "Pam";
-        myStudent2.lastName = "Beasley ";
-        myStudent2.major = "Art";
-        myStudent2.gpa = 2.5;
-        myStudent2.age  = 23;
-        myStudent2.onProbation = true;
-
-        System.out.println(myStudent2.age);
-
-
-
-
+        Cat myCat = new Cat();
+        myCat.meow();
+        myCat.name = "Oliver";
+        myCat.age = 8;
+        System.out.println("My new cat name is " +myCat.name +" and it is " +myCat.age +" years old.");
 
 
     }
